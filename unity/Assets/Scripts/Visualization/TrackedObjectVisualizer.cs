80e196ec82f7d9a3fd04ee67b3d00586047dadc4
Juan Fonseca <juan@juanfonseca.dev>
2026-09-10 21:22:15 +0200

    feat: visualize tracked object trajectories
    Closes #3

--- unity/Assets/Scripts/Visualization/TrackedObjectVisualizer.cs
+++ unity/Assets/Scripts/Visualization/TrackedObjectVisualizer.cs
@@ -17,13 +17,26 @@ namespace XrMotionDataExplorer.Visualization
 
         private SpatialSample _activeSample;
 
+        private LineRenderer _trajectory;
 
         private void Start()
         {
             _objectSamples = dataLoader.Dataset.Where(x=>x.ObjectId == ObjectId).OrderBy(x=>x.Timestamp).ToList();
-            if (_objectSamples.Count == 0) return;
+            if (_objectSamples.Count == 0)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
             _activeSample = _objectSamples[0];
             transform.position = new Vector3( _activeSample.X, _activeSample.Y, _activeSample.Z);
+
+            if (_objectSamples.Count == 1)
+            {
+                enabled = false;
+                return;
+            }
+
+            CreateTrajectory();
         }
 
         private void LateUpdate()
@@ -37,6 +50,24 @@ namespace XrMotionDataExplorer.Visualization
         public SpatialSample ResolveSample(double timestamp)
         {
             return _objectSamples.FindLast(x => x.Timestamp <= timestamp);
+        }
+
+        private void CreateTrajectory()
+        {
+            GameObject trajectoryObject= new GameObject();
+            trajectoryObject.transform.SetParent(transform);
+            trajectoryObject.name = $"{ObjectId}_trajectory";
+            _trajectory = trajectoryObject.AddComponent<LineRenderer>();
+            _trajectory.useWorldSpace = true;
+            _trajectory.material = new Material(Shader.Find("Sprites/Default"));
+            _trajectory.widthMultiplier = 0.01f;
+
+            Vector3[] positions = _objectSamples
+                .Select(x => new Vector3(x.X, x.Y, x.Z))
+                .ToArray();
+
+            _trajectory.positionCount = positions.Length;
+            _trajectory.SetPositions(positions);
         }
     }
 }
